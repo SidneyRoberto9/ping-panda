a96439754f894a775acb77ffa62cf2d6b0540524
Sidney Roberto <sidneyrpsilva@gmail.com>
2024-12-13 12:48:48 -0300

    🏗️ build: remove same erros in dploy

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -11,7 +11,7 @@ export async function POST(req: Request) {
   const event = stripe.webhooks.constructEvent(
     body,
     signature ?? "",
-    process.env.STRIPE_SECRET_KEY ?? ""
+    process.env.STRIPE_WEBHOOK_SECRET ?? ""
   )
 
   if (event.type === "checkout.session.completed") {
